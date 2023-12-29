b337e55da9dc5b66790312161d88a0d48394c107
Eric <efayaesteban@gmail.com>
2023-12-29 11:21:25 +0100

    Tornem a fa dos commits

--- Poole/Poole.c
+++ Poole/Poole.c
@@ -1,33 +1,22 @@
 #include "Poole.h"
 #include "config.h"
-
-
-
 Poole *poolete;
 int numUsuaris;//Hem de saber cuantradas usuaris estan conectats en el servidor(discovery)
 int sockfd_poole_server;
 int max_sd ;
 fd_set master_set;
-
 int *clientrada_sockets; // Array dinamic per sockets clients (de moment no lhe ficat)
 int capacity = 1; // Capacitat inicial
 pthread_mutex_t clientrada_sockets_mutex = PTHREAD_MUTEX_INITIALIZER; // Mutex per larray
-
-
-
 void sendSongListResponse(int socket) {
     char data2[FRAME_SIZE - 3 - strlen("SONGS_RESPONSE")]; // -3 por 'type' y 'header_length'.
     char *songs = (char *)malloc(1024);
-
     listAllSongs("Files/floyd", songs);
-
     snprintf(data2, sizeof(data2), "%s", songs);
     char frame_buffer[FRAME_SIZE] = {0};
     fillFrame(frame_buffer,0x02,"SONGS_RESPONSE",data2);
-
     send(socket, frame_buffer, FRAME_SIZE, 0);//Bowman send poole
 }
-
 void sendPlayListResponse(int socket) {
     char data2[FRAME_SIZE - 3 - strlen("PLAYLISTS_RESPONSE")]; 
     char *songs = (char *)malloc(1024);
@@ -38,26 +27,20 @@ void sendPlayListResponse(int socket) {
     
     char frame_buffer[FRAME_SIZE] = {0};
     fillFrame(frame_buffer,0x02,"PLAYLISTS_RESPONSE",data2);
-
     send(socket, frame_buffer, FRAME_SIZE, 0);//Bowman send poole
 }
-
-
 void *sendFileData(void *arg) {
     FileTransferInfo *info = (FileTransferInfo *)arg;
-    printf("Depuracio: iniciant send del l'arxiu: '%s'.\n", info->filePath);
-
+ 
     int fd_file = open(info->filePath, O_RDONLY);
     if (fd_file == -1) {
         perror("Error opening file");
         free(info);
         return NULL;
     }
-
     char *header = "FILE_DATA";
     size_t header_len = strlen(header);
     size_t data_capacity = FRAME_SIZE - 3 - header_len - sizeof(int) - 1; // Espacio para ID y '&'
-
     char *buffer = (char *)malloc(data_capacity);
     if (!buffer) {
         perror("Failed to allocate memory for buffer");
@@ -65,36 +48,30 @@ void *sendFileData(void *arg) {
         free(info);
         return NULL;
     }
-
     ssize_t totalBytesSent = 0;
     ssize_t readSize;
     while ((readSize = read(fd_file, buffer, data_capacity)) > 0) {
+        usleep(4000);
         char frame_buffer[FRAME_SIZE] = {0};
-  
+
         *(int *)(frame_buffer + 3 + header_len) = info->id;
         frame_buffer[3 + header_len + sizeof(int)] = '&';
         memcpy(frame_buffer + 3 + header_len + sizeof(int) + 1, buffer, readSize);
-
       
         ssize_t frameDataSize = readSize + sizeof(int) + 1; // Tamaño de los datos en el frame
         fillFrame2(frame_buffer, 0x04, header, frame_buffer + 3 + header_len, frameDataSize);
         send(info->socket, frame_buffer, FRAME_SIZE, 0);
-
-        printf("debug: enviats %zd bytes de data en aquest frame, total de data enviada: %zd bytes.\n", readSize, totalBytesSent);
+        //printf("Received and wrote %zd bytes of data in this frame, total data received: %zd bytes\n", readSize, totalBytesSent);
         totalBytesSent += readSize;
     }
-
     if (readSize == -1) {
         perror("Error reading from the file");
     }
-
-    printf("debug:  '%s' completat. Total enviaat: %zd bytes.\n", info->filePath, totalBytesSent);
+   
     close(fd_file);
     free(info);
     return NULL;
 }
-
-
 void enviarAcknowledge(int newsock,int errorSocketOrNot) {
     char *header;
     if(errorSocketOrNot==-1 ){
@@ -108,16 +85,12 @@ void enviarAcknowledge(int newsock,int errorSocketOrNot) {
     fillFrame(frame_buffer,0x01,header," ");
     send(newsock, frame_buffer, 256, 0);//Bowman send poole
 }
-
 int handleBowmanConnection(int *newsock, int errorSocketOrNot, Frame *incoming_frame) {
-
     if (errorSocketOrNot < 0) {
         perror("Error en handleBowmanConnection");
         close(*newsock);
         return -1;
     }
-
-
     if (strcmp(incoming_frame->header, "NEW_BOWMAN") == 0) { 
         char *buffer;
         asprintf(&buffer,"New user connected: %s.\n\n...", incoming_frame->data);  
@@ -126,7 +99,6 @@ int handleBowmanConnection(int *newsock, int errorSocketOrNot, Frame *incoming_f
     
         enviarAcknowledge(*newsock, errorSocketOrNot);
     }
-
     else if (strcmp(incoming_frame->header, "LIST_SONGS") == 0)
     {
         char *buffer;
@@ -135,7 +107,6 @@ int handleBowmanConnection(int *newsock, int errorSocketOrNot, Frame *incoming_f
         free(buffer);
         sendSongListResponse(*newsock);
     }
-
     else if (strcmp(incoming_frame->header, "LIST_PLAYLISTS") == 0)
     {
         char *buffer;
@@ -144,17 +115,14 @@ int handleBowmanConnection(int *newsock, int errorSocketOrNot, Frame *incoming_f
         free(buffer);
         sendPlayListResponse(*newsock);
     }
-
     else if (strcmp(incoming_frame->header, "DOWNLOAD_SONG") == 0) //TODO    A total of 2 songs will be sent. AQUEST PRINTF,SA DE CONTAR EL NUMERO DE CANSONS O ALGO AIXI K SENVIEN
     {
         char path_found[PATH_MAX];
         char *song_name = incoming_frame->data; 
-
         char *buffer;
         asprintf(&buffer,"New request – %s wants to download %s\n Sending %s to %s\n\n", incoming_frame->data,song_name,song_name,incoming_frame->data);  
         write(STDOUT_FILENO, buffer, strlen(buffer));   
         free(buffer);
-
         int found = findSongInDirectory("Files/floyd", song_name, path_found);
         if (found) {
             struct stat st;
@@ -163,12 +131,10 @@ int handleBowmanConnection(int *newsock, int errorSocketOrNot, Frame *incoming_f
                 return -1;
             }
             int file_size = st.st_size;
-
             char *md5sum = calculateMD5(path_found);        // Calcular el MD5SUM
             if (md5sum == NULL) {
                 return -1;
             }
-
             int data_info_size = strlen(song_name) + 20 + 32 + 2;         // calculem tamany de data amb els 3 components
             char *data_info = malloc(data_info_size);
             if (data_info == NULL) {
@@ -176,12 +142,9 @@ int handleBowmanConnection(int *newsock, int errorSocketOrNot, Frame *incoming_f
                 free(md5sum);
                 return -1;
             }
-
             int idNumRandom = 0 + rand() % 999;
             snprintf(data_info, data_info_size, "%s&%d&%s&%d", song_name, file_size, md5sum,idNumRandom);        //trama pel primer frame, el del md5
-
             char frame_buffer[FRAME_SIZE];        // Enviar la trama
-
             fillFrame(frame_buffer, 0x04, "NEW_FILE", data_info);
             send(*newsock, frame_buffer, FRAME_SIZE, 0); //aquest l'envia bé
 
@@ -191,11 +154,9 @@ int handleBowmanConnection(int *newsock, int errorSocketOrNot, Frame *incoming_f
                 // Manejar error
                 return -1;
             }
-
             transferInfo->socket = *newsock;
             strncpy(transferInfo->filePath, path_found, PATH_MAX);
             transferInfo->id = idNumRandom;
-
             pthread_t fileTransferThread;
             if (pthread_create(&fileTransferThread, NULL, sendFileData, transferInfo) != 0) {
                 perror("Error creating file transfer thread");
@@ -203,7 +164,6 @@ int handleBowmanConnection(int *newsock, int errorSocketOrNot, Frame *incoming_f
                 free(transferInfo);
                 return -1;
             }
-
             free(data_info); 
             free(md5sum);    
         } 
@@ -216,38 +176,26 @@ int handleBowmanConnection(int *newsock, int errorSocketOrNot, Frame *incoming_f
         asprintf(&buffer,"Result MD5SUM– %s\n", incoming_frame->header);  
         write(STDOUT_FILENO, buffer, strlen(buffer));   
         free(buffer);    
-
-
         char frame_buffer[FRAME_SIZE];        // Enviar la trama ACK INVENTADA
-
         fillFrame(frame_buffer, 0x08, "FINISH", "");
         send(*newsock, frame_buffer, FRAME_SIZE, 0); //aquest l'envia bé
-
     }
-
     else if (strcmp(incoming_frame->header, "EXIT") == 0)
     {
         enviarAcknowledge(*newsock,errorSocketOrNot);       
         return -1;
     }
-
     free(incoming_frame->header);
     free(incoming_frame->data);
     return 0;
 }
-
-
-
 void *clientHandler(void *args) {
     ThreadArgs *threadArgs = (ThreadArgs *)args;
     int clientSocket = threadArgs->socket;
     free(threadArgs);
-
-
     while (1) {
         Frame incoming_frame;
         int errorSocketOrNot = receive_frame(clientSocket, &incoming_frame);
-
         if (errorSocketOrNot == -1) {
             break;
         }/* else if (errorSocketOrNot == 0) {
@@ -255,55 +203,43 @@ void *clientHandler(void *args) {
             break;
         }*/
         int exitOrNot = handleBowmanConnection(&clientSocket, errorSocketOrNot, &incoming_frame);
-
-
         if (exitOrNot == -1) {
             break;
         }
     }
-
     close(clientSocket);
     pthread_exit(NULL);
 }
-
-
 void connectToBowman(Poole *poolete) {
     uint16_t poole_port = poolete[0].portPoole;
-
     // Verifica que el puerto sea válido.
     if (poole_port < 1) {
         write(STDOUT_FILENO, "Error: Invalid port number(s)\n", sizeof("Error: Invalid port number(s)\n"));   
         exit(EXIT_FAILURE);
     }
-
     // Crea un socket para el servidor Poole.
     int sockfd_poole_server = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd_poole_server < 0) {
         perror("Error creating sockets");
         exit(EXIT_FAILURE);
     }
-
     // Configura la dirección y el puerto del servidor.
     struct sockaddr_in bowman_addr;
     memset(&bowman_addr, 0, sizeof(bowman_addr));
     bowman_addr.sin_family = AF_INET;
     bowman_addr.sin_addr.s_addr = INADDR_ANY;
     bowman_addr.sin_port = htons(poole_port);
-
     
     if (bind(sockfd_poole_server, (struct sockaddr *)&bowman_addr, sizeof(bowman_addr)) < 0) {
         perror("Error binding socket");
         exit(EXIT_FAILURE);
     }
-
     
     if (listen(sockfd_poole_server, 5) < 0) {
         perror("Error listening on socket");
         exit(EXIT_FAILURE);
     }
-
     while (1) {
-
         struct sockaddr_in c_addr;
         socklen_t c_len = sizeof(c_addr);
         int newsock = accept(sockfd_poole_server, (struct sockaddr *)&c_addr, &c_len);
@@ -320,7 +256,6 @@ void connectToBowman(Poole *poolete) {
         }
         args->socket = newsock;
  
-
         //crear thread per gestionar connexio
         pthread_t thread_id;
         if (pthread_create(&thread_id, NULL, clientHandler, (void *)args) != 0) {
@@ -334,7 +269,6 @@ void connectToBowman(Poole *poolete) {
     // Libera los recursos al final.
     free(clientrada_sockets);
     close(sockfd_poole_server);
-
 }
     
 void freeAndClose(Poole *poolete,int numUsuaris){ 
@@ -347,7 +281,6 @@ void freeAndClose(Poole *poolete,int numUsuaris){
     }
     free(poolete);
 }
-
 void kctrlc(){ 
     freeAndClose(/*poole_frame,*/poolete,numUsuaris);
     for (int i = 0; i <= max_sd; ++i) {// sockets amb arrays ja afegits i creats
@@ -357,15 +290,12 @@ void kctrlc(){
         clientrada_sockets[i] = 0;
     }
     close(sockfd_poole_server);
-
     //logout(); //SIGNAL CONTROL+C TODO: S'HAURA DE FER VARIABLE GLOBAL  crec EL FD SOCKET :int sockfd_poole 
 }
-
 int main(int argc, char *argv[]){
     //Poole *poolete;
     //int numUsuaris;//Hem de saber cuantradas usuaris estan conectats en el servidor(discovery)
     signal(SIGINT, kctrlc);
-
     if (argc != 2){
         printF("ERROR: Incorrect number of argumentradas\n");
         return -1;
@@ -375,24 +305,18 @@ int main(int argc, char *argv[]){
     if (poolete == NULL){
         return -2;
     }
-
     char *userName2 = poolete[0].fullName;
     char *ipPoole = poolete[0].ipPoole;
     uint16_t portPoole = poolete[0].portPoole;
-
     char data2[FRAME_SIZE - 3 - strlen("NEW_POOLE")]; // -3 por 'type' y 'header_length'.
     snprintf(data2, sizeof(data2), "%s&%s&%u", userName2, ipPoole, portPoole);
-
-
     char frame_buffer[FRAME_SIZE] = {0};
     fillFrame(frame_buffer,0x01,"NEW_POOLE",data2);
     
-
     struct sockaddr_in server_addr;    //sockaddr_in: struct defineix l’estructura que permet configurar diversos paràmetres del socket com IP, port
     memset(&server_addr, 0, sizeof(server_addr));//Inicialitza,fica 0s a l'estructura
     server_addr.sin_family = AF_INET;//tipus de familia de socket es tracta
     server_addr.sin_port = htons(poolete[0].portDiscovery);//(Host To Network Short) Converteix port a big endian
-
     if (inet_pton(AF_INET, poolete[0].ipDiscovery, &server_addr.sin_addr) < 0) { //Converteix representradaació en text de la ip a l’equivalentrada binari (IPv4)
         perror("Invalid address/ Address not supported");
         exit(EXIT_FAILURE); 
@@ -404,18 +328,15 @@ int main(int argc, char *argv[]){
         exit(EXIT_FAILURE);
         //fer free de memoria dinamica
     }
-
     if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         perror("Connection Failed");
         exit(EXIT_FAILURE);
         //fer free de memoria dinamica
         close(sockfd); //Fas close per si acas
     }
-
     send(sockfd, frame_buffer, FRAME_SIZE, 0);
     char info[256];
     Frame frameAcknoledge;
-
     read(sockfd, info, 256);
     printaAcknowledge(info,&frameAcknoledge);
     
@@ -424,9 +345,7 @@ int main(int argc, char *argv[]){
     asprintf(&buffer,"\nReading configuration file\nConnecting %s Server to the system..\nConnected to HAL 9000 System, ready to listen to Bowmans petitions\n\nWaiting for connections...\n\n", userName2);  
     write(STDOUT_FILENO, buffer, strlen(buffer));   
     free(buffer);
-
     connectToBowman(poolete);
-
     freeAndClose(/*poole_frame,*/poolete,numUsuaris);
     return 0;  
 }
